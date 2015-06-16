39239f08f3b4db5961e05815f46b22e72706baa5
Max Tedford <max.tedford@gmail.com>
2015-06-16 10:53:26 -0600

    reconcile hound issues

--- spec/features/unauth_user_views_items_index_spec.rb
+++ spec/features/unauth_user_views_items_index_spec.rb
@@ -1,12 +1,4 @@
-require 'rails_helper'
-
-# describe "unauthenticated user", type: :feature do
-#   it "shows an items index page to an unauthenticated user" do
-#     visit "/items"
-#
-#     expect(page).to have_content("Menu")
-#   end
-# end
+require "rails_helper"
 
 feature "an unauthenticated user" do
   scenario "will see an items index page" do
@@ -17,23 +9,43 @@ feature "an unauthenticated user" do
   end
 
   scenario "the index page has all items listed" do
-    item = Item.create(title: "Unicorn Roll", description: "Gooooood", price: 8, status: "active")
+    Item.create(title: "Unicorn Roll",
+                description: "Gooooood", 
+                price: 8, 
+                status: "active")
+    
     visit items_path
+    
     within "table" do
       expect(page).to have_content("Unicorn Roll")
     end
   end
 
   scenario "the index page displays items in rows of four" do
-    Item.create(title: "Unicorn Roll", description: "Gooooood", price: 8, status: "active")
-    Item.create(title: "Medusa Hair Pasta", description: "Gooooood", price: 8, status: "active")
-    Item.create(title: "Pixie Bread Stix", description: "Gooooood", price: 8, status: "active")
-    Item.create(title: "Phoenix Spicy Curry", description: "Gooooood", price: 8, status: "active")
-    Item.create(title: "Titan Burger", description: "Gooooood", price: 8, status: "active")
+    Item.create(title: "Unicorn Roll", 
+                description: "Gooooood", 
+                price: 8, 
+                status: "active")
+    Item.create(title: "Medusa Hair Pasta", 
+                description: "Gooooood", 
+                price: 8, 
+                status: "active")
+    Item.create(title: "Pixie Bread Stix", 
+                description: "Gooooood", 
+                price: 8, 
+                status: "active")
+    Item.create(title: "Phoenix Spicy Curry", 
+                description: "Gooooood", 
+                price: 8, 
+                status: "active")
+    Item.create(title: "Titan Burger", 
+                description: "Gooooood", 
+                price: 8, 
+                status: "active")
 
     visit items_path
+    
     within "table" do
-      save_and_open_page
       Item.all.each do |item|
         expect(page).to have_content(item.title)
       end
