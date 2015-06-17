c3a6ddb81615aea0ea1939560370cc51a30cfd4e
Max Tedford <max.tedford@gmail.com>
2015-06-16 20:57:59 -0600

    WIP - troubleshoot error message

--- spec/features/unauth_user_views_items_index_spec.rb
+++ spec/features/unauth_user_views_items_index_spec.rb
@@ -1,57 +1,53 @@
 require "rails_helper"
 
 feature "an unauthenticated user" do
-  context "will see an item" do
     
-    scenario "will see an items index page" do
-      visit items_path
-      within "h1" do
-        expect(page).to have_content("Menu")
-      end
+  scenario "will see an items index page" do
+    visit items_path
+    within "h1" do
+      expect(page).to have_content("Mythical Eatures")
     end
+  end
 
-    scenario "will see that the index page has all items listed" do
-      Item.create(title: "Unicorn Roll",
-        description: "Gooooood",
-        price: 8,
-        status: "active")
+  scenario "will see that the index page has all items listed" do
+    Item.create(title: "Unicorn Roll",
+      description: "Gooooood",
+      price: 8,
+      status: "active")
 
-      visit items_path
+    visit items_path
 
-      within "table" do
-        expect(page).to have_content("Unicorn Roll")
-      end
+    within ".large-3" do
+      expect(page).to have_content("Unicorn Roll")
     end
+  end
 
-    scenario "will see that the index page displays items in rows of four" do
-      Item.create(title: "Unicorn Roll",
-        description: "Gooooood",
-        price: 8,
-        status: "active")
-      Item.create(title: "Medusa Hair Pasta",
-        description: "Gooooood",
-        price: 8,
-        status: "active")
-      Item.create(title: "Pixie Bread Stix",
-        description: "Gooooood",
-        price: 8,
-        status: "active")
-      Item.create(title: "Phoenix Spicy Curry",
-        description: "Gooooood",
-        price: 8,
-        status: "active")
-      Item.create(title: "Titan Burger",
-        description: "Gooooood",
-        price: 8,
-        status: "active")
+  scenario "will see that the index page displays items in rows of four" do
+    Item.create(title: "Unicorn Roll",
+      description: "Gooooood",
+      price: 8,
+      status: "active")
+    Item.create(title: "Medusa Hair Pasta",
+      description: "Gooooood",
+      price: 8,
+      status: "active")
+    Item.create(title: "Pixie Bread Stix",
+      description: "Gooooood",
+      price: 8,
+      status: "active")
+    Item.create(title: "Phoenix Spicy Curry",
+      description: "Gooooood",
+      price: 8,
+      status: "active")
+    Item.create(title: "Titan Burger",
+      description: "Gooooood",
+      price: 8,
+      status: "active")
 
-      visit items_path
+    visit items_path
 
-      within "table" do
-        Item.all.each do |item|
-          expect(page).to have_content(item.title)
-        end
-      end
+    Item.all.each do |item|
+      expect(page).to have_content(item.title) 
     end
   end
 end
