bf51a01e817f06317895f8b9b4ac5394bb473c7d
Max Tedford <max.tedford@gmail.com>
2015-06-17 13:24:56 -0600

    add unauthenticated user adds items to cart functionality

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,7 +3,6 @@ class CartsController < ApplicationController
   def create
     item = Item.find(params[:item_id])
     @cart.add_item(item.id)
-    byebug
     session[:cart] = @cart.contents
     # flash[:notice] = "You now have #{pluralize(@cart.count_of(item.id), item.title)}."
     redirect_to items_path
