d6b66fffe52077eb08c7d5b954a40d75144fca75
Max Tedford <max.tedford@gmail.com>
2015-06-17 11:39:03 -0600

    add item objects to cart view

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,6 +3,7 @@ class CartsController < ApplicationController
   def create
     item = Item.find(params[:item_id])
     @cart.add_item(item.id)
+    byebug
     session[:cart] = @cart.contents
     # flash[:notice] = "You now have #{pluralize(@cart.count_of(item.id), item.title)}."
     redirect_to items_path
