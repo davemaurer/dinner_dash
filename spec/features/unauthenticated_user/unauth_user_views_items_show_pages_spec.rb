7d64f4e3a3ef7f86c4de38aebb2730c8bb8daaf6
Max Tedford <max.tedford@gmail.com>
2015-06-17 19:42:18 -0600

    add item show pages for each item from items index page

--- spec/features/unauthenticated_user/unauth_user_views_items_show_pages_spec.rb
+++ spec/features/unauthenticated_user/unauth_user_views_items_show_pages_spec.rb
@@ -2,19 +2,37 @@ require "rails_helper"
 
 feature "an authenticated user" do
   
-  let(:unicorn) { Item.create(title: "Unicorn Pie", description: "Good", price: 8) }
-  let(:medusa) { Item.create(title: "Medusa Hair Pasta", description: "Good", price: 8) }
-  let(:titan) { Item.create(title: "Titan Burger", description: "Good", price: 8) }
+  before(:each) do
+    Item.create(title: "Unicorn Pie", description: "Good", price: 8, status: "active")
+    Item.create(title: "Medusa Hair Pasta", description: "Good", price: 8, status: "active")
+    Item.create(title: "Titan Burger", description: "Good", price: 8, status: "active")
+  end
   
-  context "will see item show pages" do
-    scenario "user will see links each item's show page" do
-      visit items_path
-      
-      within ".large-3" do
-        expect(page).to have_link("Unicorn Pie")
-        expect(page).to have_link("Medusa Hair Pasta")
-        expect(page).to have_link("Titan Burger")
-      end
-    end
+  scenario "will see links to each item's show page" do
+    visit items_path
+
+    expect(page).to have_link("Unicorn Pie")
+    expect(page).to have_link("Medusa Hair Pasta")
+    expect(page).to have_link("Titan Burger")
+  end
+  
+  scenario "can click to each item's show page" do
+    visit items_path
+    click_link "Unicorn Pie"
+    
+    expect(page).to have_content("Unicorn Pie")
+    expect(page).to have_content("$8")
+    expect(page).to have_content("Good")
+  end
+  
+  scenario "can click back to return to the items index page" do
+    visit items_path
+    click_link "Unicorn Pie"
+    click_link "<< Back"
+    
+    expect(current_path).to eq(items_path)
+    expect(page).to have_link("Unicorn Pie")
+    expect(page).to have_link("Medusa Hair Pasta")
+    expect(page).to have_link("Titan Burger")
   end
 end
