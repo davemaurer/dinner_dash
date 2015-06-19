0cba226d32cdae1fe2af8699dcebdf2b696499f7
margie <ly.margarett@gmail.com>
2015-06-19 14:16:20 -0600

    all tests pass after merging from master

--- spec/features/unauthenticated_user/submit_order_spec.rb
+++ spec/features/unauthenticated_user/submit_order_spec.rb
@@ -1,43 +1,45 @@
 require "rails_helper"
 
 feature "an unauthenticated user" do
-  
+
   before(:each) do
+    category = Category.create(name: "new cat")
     @unicorn = Item.create(title: "Unicorn Pie",
                            description: "Good",
                            price: 8,
-                           status: "active")
+                           status: "active",
+                           categories: [category])
     User.create(full_name: "John Doe",
                 user_name: "john",
                 email: "john@john.com",
                 password: "password")
   end
-  
+
   scenario "can submit an order" do
     visit items_path
     first(:button, "Add To Cart").click
     within ".right" do
       first(:link).click
     end
-    
+
     expect(current_path).to eq(cart_path)
     expect(page).to have_content("Unicorn Pie")
-    
+
     click_button "Checkout"
-    
+
     expect(current_path).to eq(login_path)
-    
+
     fill_in "Username", with: "john"
     fill_in "Password", with: "password"
     click_button "Login"
-    
+
     expect(current_path).to eq(cart_path)
-    
+
     click_button "Checkout"
-    
+
     expect(page).to have_content("Thank you for your order, John Doe!")
   end
-  
+
   scenario "can see their order details on the order confirmation page" do
     visit items_path
     first(:button, "Add To Cart").click
@@ -57,7 +59,7 @@ feature "an unauthenticated user" do
     expect(page).to have_content("Order Status: pending")
     expect(page).to have_content("Order Submitted:")
   end
-  
+
   scenario "can click to a particular item's show page from his/her order confirmation page" do
     visit items_path
     first(:button, "Add To Cart").click
@@ -70,7 +72,7 @@ feature "an unauthenticated user" do
     click_button "Login"
     click_button "Checkout"
     click_link "Unicorn Pie"
-    
+
     expect(current_path).to eq(item_path(@unicorn))
     expect(page).to have_content("Unicorn Pie")
   end
--- spec/features/unauthenticated_user/unauth_cart_login_spec.rb
+++ spec/features/unauthenticated_user/unauth_cart_login_spec.rb
@@ -1,32 +1,33 @@
 require "rails_helper"
 
 RSpec.feature "the unauthenticated user", type: :feature do
-  
+
   before(:each) do
-    Item.create(title: "Unicorn Pie", description: "Good", price: 8, status: "active")
+    category = Category.create(name: "new cat")
+    Item.create(title: "Unicorn Pie", description: "Good", price: 8, status: "active", categories: [category])
   end
-  
+
   scenario "is prompted to login if he/she hasn't done so at checkout" do
     visit items_path
     first(:button, "Add To Cart").click
     within ".right" do
       find(:link).click
     end
-    
+
     expect(current_path).to eq(cart_path)
     expect(page).to have_content("Unicorn Pie")
-    
+
     click_button "Checkout"
-    
+
     expect(current_path).to eq(login_path)
   end
-  
+
   scenario "when I login from the checkout path and click 'login' I return to the cart page" do
     User.create(full_name: "John Doe",
                 user_name: "john",
                 email: "john@john.com",
                 password: "password")
-    
+
     visit items_path
     first(:button, "Add To Cart").click
     within ".right" do
@@ -42,7 +43,7 @@ RSpec.feature "the unauthenticated user", type: :feature do
     fill_in "Username", with: "john"
     fill_in "Password", with: "password"
     click_button "Login"
-    
+
     expect(current_path).to eq(cart_path)
     expect(page).to have_content("Unicorn Pie")
     expect(page).to have_button("Checkout")
