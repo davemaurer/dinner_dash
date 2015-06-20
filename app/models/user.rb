ba00ed164763706e60b4f1fbc4f081fdb241520a
Max Tedford <max.tedford@gmail.com>
2015-06-19 22:30:10 -0600

    Add seed data and finish user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :full_name, presence: true
-  validates :user_name, length: {minimum: 2, maximum: 32}
+  validates :user_name, length: {minimum: 2, maximum: 32}, :if => :username_present? 
   validates :password, presence: true
   validates :email,
     presence: true,
@@ -10,4 +10,10 @@ class User < ActiveRecord::Base
     format: { :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/ }
 
   enum role: %w[default admin]
+  
+  private
+  
+  def username_present?
+    !user_name.blank?
+  end
 end
