dabef2a2b6bd43b7b6f02356e1bad85d0e149d1a
Max Tedford <max.tedford@gmail.com>
2015-06-19 20:23:40 -0600

    add validations for order model

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,15 @@
 class OrdersController < ApplicationController
   
   def create
-    @order = Order.new(status: "pending", 
-                       total_price: total_price,
-                       user_id: session[:user_id])
+    @items = []
     @cart.contents.each do |item_id, quantity|
-      @order.order_items.build(:item_id => item_id, :quantity => quantity)
+      quantity.times do
+        @items << (Item.find(item_id))
+      end
     end
+    @order = Order.new(total_price: total_price,
+                       user_id: session[:user_id],
+                       items: @items)
     @order.save!
     @cart.reset
     flash[:notice] = "Thank you for your order, #{@order.user.full_name}!"
