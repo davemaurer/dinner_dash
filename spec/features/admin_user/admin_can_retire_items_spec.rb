e03ef7bce42a06087433867c90521f2e6bdd609f
Max Tedford <max.tedford@gmail.com>
2015-06-21 18:35:38 -0600

    refactor admin item retire test suite

---
+++ spec/features/admin_user/admin_can_retire_items_spec.rb
@@ -0,0 +1,50 @@
+require "rails_helper"
+
+RSpec.feature "the admin can retire an item" do
+  
+    let(:category)  {Category.create(name: "Category")}
+  
+    let(:admin)  {User.create(full_name: "Admin",
+                     email: "admin@admin.com",
+                     password: "password",
+                     role: 1)}
+    let(:user)   {User.create(full_name: "User",
+                     email: "user@user.com",
+                     password: "password",
+                     role: 0)}
+    
+    let(:active)   {Item.create(title: "Active Item",
+                       description: "I'm active!",
+                       price: 9.99,
+                       categories: [category])}
+    let(:retired)  {Item.create(title: "Retired Item",
+                       description: "I'm retired!",
+                       price: 8.99,
+                       categories: [category],
+                       status: "retired")}
+    
+    before(:each) do
+      category
+      admin
+      user
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    end
+  
+  scenario "and its status chances to 'retired'" do
+    active
+    
+    visit admin_items_path
+    
+    expect(page).to have_content "Active Item"
+    
+    first(:button, "Edit Item").click
+    
+    expect(current_path).to eq(edit_admin_item_path(active))
+    expect(page).to have_content("Status")
+
+    select "retired", :from => "item[status]"
+    click_button "Finished"
+    
+    expect(page).to have_content("This item has been retired")
+  end
+end
