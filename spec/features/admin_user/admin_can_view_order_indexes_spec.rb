82ada2f4416ff709973d409c257b2da7ab8d9170
margie <ly.margarett@gmail.com>
2015-06-24 14:00:43 -0600

    fix order spec

--- spec/features/admin_user/admin_can_view_order_indexes_spec.rb
+++ spec/features/admin_user/admin_can_view_order_indexes_spec.rb
@@ -51,7 +51,7 @@ RSpec.feature "the admin can visit an order page" do
   scenario "and click to view an order show page" do
     visit admin_order_path(order1)
 
-    expect(page).to have_content("Order 1")
+    expect(page).to have_content("Order #{order1.id}")
   end
 
   scenario "and sees the date and time the order was placed" do
