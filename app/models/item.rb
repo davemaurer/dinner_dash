4cd1070f9d55407d3e5da4a047f68602f609f1db
margie <ly.margarett@gmail.com>
2015-06-24 22:32:27 -0600

    refactored items controller

--- app/models/item.rb
+++ app/models/item.rb
@@ -25,6 +25,8 @@ class Item < ActiveRecord::Base
   has_many :order_items
   has_many :orders, through: :order_items
 
+  scope :active_items, -> { where(status: "active") }
+
   def has_category
     if categories.to_a.count < 1
       errors.add(:categories, "item must have at least one category")
